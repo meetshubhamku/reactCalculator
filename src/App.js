dc02f37381b7a4e464cad3eae041ce7bc0ccd05d
Shubham Kumar <meetshubhamku@gmail.com>
2021-07-07 15:40:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Container,
+  Typography,
+  AppBar,
+  Toolbar,
+  Grid,
+} from "@material-ui/core";
+import useStyles from "./Styles";
+import { evaluate } from "mathjs";
 
-function App() {
+export default function App() {
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState(0);
+
+  const classes = useStyles();
+
+  const handleClick = (event) => {
+    setExpression(expression + event.target.value);
+  };
+  const evaluateResult = () => {
+    setResult(evaluate(expression));
+  };
+  const clearResult = () => {
+    setExpression("");
+    setResult(0);
+  };
+  const buttonKeys = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+  const operationKeys = ["+", "-", "*", "/"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar position="relative">
+        <Toolbar>Kalc</Toolbar>
+      </AppBar>
+      <Container maxWidth="xs">
+        <Typography variant="h2">{result ? result : "0"}</Typography>
+        <Typography variant="h6">
+          {expression ? expression : "Expression"}
+        </Typography>
+        <Grid container spacing={1}>
+          {/* Looping All digit buttons */}
+          {buttonKeys.map((item, index) => {
+            return (
+              <Grid item key={index} xs={4} sm={4} md={4}>
+                <button
+                  className={classes.button}
+                  onClick={handleClick}
+                  color="primary"
+                  value={item}
+                >
+                  {item}
+                </button>
+              </Grid>
+            );
+          })}
+          {/* Looping All operation buttons */}
+          {operationKeys.map((item, index) => {
+            return (
+              <Grid item key={index} xs={4} sm={4} md={4}>
+                <button
+                  className={classes.button}
+                  onClick={handleClick}
+                  color="secondary"
+                  value={item}
+                >
+                  {item}
+                </button>
+              </Grid>
+            );
+          })}
+          {/* Equal to (=) button */}
+          <Grid item xs={4} sm={4} md={4}>
+            <button
+              className={classes.button}
+              onClick={evaluateResult}
+              color="secondary"
+            >
+              =
+            </button>
+          </Grid>
+          {/* clear button */}
+          <Grid item xs={4} sm={4} md={4} lg={4}>
+            <button
+              className={classes.button}
+              onClick={clearResult}
+              color="primary"
+            >
+              Clear
+            </button>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 }
-
-export default App;
---
+++ src/Styles.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+  button: {
+    padding: "10px 0px",
+    width: "65px",
+    borderRadius: "4px",
+    border: "none",
+    backgroundColor: "#4C4C6D",
+    fontWeight: "bold",
+    fontSize: "1.1rem",
+    color: "#B8DFD8",
+  },
+}));
+export default useStyles;
